892672d0d1e716fa4de91f700d959f8a6e6f27c5
It21822612 <110956264+It21822612@users.noreply.github.com>
2022-09-25 13:08:13 +0530

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,9 +1,16 @@
 class Student {
   // private section
+private:
+	int studentID;
   //    int studentId
+	char name[20];
   //    name <- 20 charcters
   
   // public section
-  //    assignDetails() method declaration
+public:
+	//    assignDetails() method declaration
+	void assignDetails(int sid, char sname[20]);
+	
   //    display() method declaration
+	void display();
 };
