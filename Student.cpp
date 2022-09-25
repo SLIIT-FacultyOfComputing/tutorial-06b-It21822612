187f9e760a29b226851539ed0b3ce1f3acbc9a07
It21822612 <110956264+It21822612@users.noreply.github.com>
2022-09-25 13:07:50 +0530

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 
 // Assign studentId and name
-Student::assignDetails() {
-  
+Student::assignDetails(int sid, char sname[20]) {
+	studentID = sid;
+	name[20] = sname[20];
 }
 
 // Display StudentId and Name
 Student::display() {
-  
+	std::cout << "student ID: " << studentID << std::endl;
+	std::cout << "name: " << name[20] << std::endl;
+
 }
